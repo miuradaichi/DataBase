d49fc00236bbab6f71ab07f2ce06948aa4e13163
miuradaichi <miura.kaza@gmail.com>
2021-06-19 14:58:16 +0900

    JS_test-commit

--- test__miura/test.js
+++ test__miura/test.js
@@ -1,11 +1,28 @@
 const array = [2, 4, 7, 5, 4, 3, 8];
 
-// console.log(array);
-
-// const array = [8, 10, 5, 3, 2];
-
-const result = array.filter(function (item) {
-  return item == array;
+const result = array.filter(function (foo,sake,array) {
+  return array.indexOf(foo) ===sake;
 });
 
 console.log(result);
+
+/*------------------------------------*/
+
+// function leapYear(year) {
+//   if (year % 4 === 0 && year % 100 !== 0||year%400===0) {
+//     return console.log(year)
+//   } return console.log(`${year}はうるう年ではありません`)
+// }
+
+// leapYear(2020);
+// leapYear(2021);
+
+/*-----------------------------------------------*/
+
+const leapYear = [2020, 2021];
+
+leapYear.forEach(function (year) {
+  if (year % 4 === 0 && year % 100 !== 0||year%400===0) {
+    return console.log(year)
+  } return console.log(`${year}はうるう年ではありません`)
+})
